992a09951d8f43f82c0638967f4addbfbee47472
yixingfu <yxfu93@hotmail.com>
2020-04-01 16:10:56 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,6 +81,8 @@ fi
 echo $new
 echo $(head Project.toml)
 sed -i "s/[0-9]\{1,2\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}/${new#v}/" Project.toml
+echo $(head Project.toml)
+
 
 # set outputs
 echo ::set-output name=new_tag::$new
